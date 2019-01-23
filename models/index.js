7dd42dc9e020b1586129315fc04fcaa993b7f483
Dylan White <dylanawhite11@gmail.com>
2019-01-23 11:02:10 -0600

    modifying userInfo schema

--- models/index.js
+++ models/index.js
@@ -5,5 +5,5 @@
 //collect and export all of your models together here
 
 module.exports = {
-    Example: require("./example")
+    User: require("./userInfo")
 };
--- models/userInfo.js
+++ models/userInfo.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userInfoSchema = new Schema({
-
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userName: { type: String, required: true },
+    image: {type: String, required: true },
+    portfolio: String,
+    date: { type: Date, default: Date.now }
 });
 
 const User = mongoose.model("User", userInfoSchema);

f1e33c1135714d4ac723185521419bd46216d804
Dylan White <dylanawhite11@gmail.com>
2019-01-23 13:47:52 -0600

    modified profile schema

--- models/userInfo.js
+++ models/userInfo.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema;
 const userInfoSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
     image: {type: String, required: true },
-    portfolio: String,
+    portfolios: [String],
+    bio: String,
+    instruments: [String],
+    genres: [String],
+    // Show how long someone has been a user, if necessary
     date: { type: Date, default: Date.now }
 });
 

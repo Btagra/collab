1c2a714f83e347ca51178539719c767fad45a363
Dylan White <dylanawhite11@gmail.com>
2019-01-23 14:07:04 -0600

    modifying inputs

--- client/src/pages/Profile.js/Profile.js
+++ client/src/pages/Profile.js/Profile.js
@@ -1,31 +1,9 @@
 import React from "react";
+import Input from "../../components/Input";
 
 function ProfileForm({ q, handleInputChange, handleFormSubmit}) {
     return(
-        <form>
-            <div className="form-group">
-                <label htmlFor="Query">Profile Image: </label>
-                <input 
-                    className="form-control" 
-                    id="Title" 
-                    type="text" 
-                    value={q}
-                    placeholder=""
-                    name="image"
-                    onChange={handleInputChange}
-                    required 
-                />
-            </div>
-            <div className="pull-right">
-                <button 
-                    onClick={handleFormSubmit}
-                    type="submit"
-                    // className="btn btn-lg btn-danger float-right"
-                >
-                    Search
-                </button>
-            </div>
-        </form>
+        <Input />
     );
 };
 

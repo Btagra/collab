938d9e9cd0bed0b8c41d524d9d7593a84e019869
Dylan White <dylanawhite11@gmail.com>
2019-01-24 14:26:25 -0600

    created working form route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import dotenv from "dotenv";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Form from "./pages/Form";
 import NoMatch from "./pages/NoMatch";
 dotenv.config();
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/form" component={Form} />
       <Route component={NoMatch} />
     </Switch>
   </Router>
---
+++ client/src/pages/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+class Form extends Component {
+    render() {
+        return (
+            <>
+                <Container>
+                    <Row>
+                        <Col size="md-12">
+                            <p>hello</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    };
+}
+
+export default Form;

a5d6dee5bcea12724f957740da0bcf3aabf2a805
Dylan White <dylanawhite11@gmail.com>
2019-01-25 13:17:54 -0600

    added accept attribute to validate that a user is submitting accepted image types

--- client/src/components/Forms/Image.js
+++ client/src/components/Forms/Image.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 
 class Image extends Component {
+    state = { selectedFile: null }
+
+    fileChangedHandler = event => {
+        this.setState({ selectedFile: event.target.files[0] });
+    }
+
+    uploadHandler = () => {
+        console.log(this.state.selectedFile);
+    }
+
     render() {
         return (
             <>
                 <label>
                     Profile Picture: 
-                    <input type="file" onChange={this.fileChangedHandler} />
+                    <input type="file" onChange={this.fileChangedHandler} accept="image/jpeg, image/png"/>
                 </label>
                 <br/>
+                {/* Once image is uploaded, they can submit it */}
                 <button onClick={this.uploadHandler}>Upload Your Image</button>
             </>
         );

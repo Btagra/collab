740880f88e64c5586d1677c8372a97b25466d7ec
Bharti <tagra.bharti@gmail.com>
2019-01-28 11:01:17 -0600

    updated import in index.js of api routes

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const notesController = require("../../controllers/profilesController");
+const profilesController = require("../../controllers/profilesController");
 
-router.route("/:uid").get(notesController.findProfiles);
-router.route("/create").post(notesController.newProfile);
+router.route("/:uid").get(profilesController.findProfiles);
+router.route("/create").post(profilesController.newProfile);
 
 module.exports = router;

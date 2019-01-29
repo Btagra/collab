1356a84cfe4157e461a0fb49372c9b8dd7bb70a1
zhakina90 <zhakina90@gmail.com>
2019-01-29 15:42:13 -0600

    Added signature schema for messages. Created a route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./users");
 const profileRoutes = require("./profiles");
-
+const messageRoutes = require("./message");
 router.use("/user", userRoutes);
 router.use("/profiles", profileRoutes);
-
+router.use("/messsages", messageRoutes);
 // For anything else, render the html page
 router.use(function (req, res) {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));

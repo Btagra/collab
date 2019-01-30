e8c69bbafaa80b84c19063aadc3a5eab0db92ef1
Bharti <tagra.bharti@gmail.com>
2019-01-30 13:09:34 -0600

    some changes

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -20,6 +20,16 @@ class Form1 extends React.Component {
         bio: '',
         instruments: [],
         selectedFile: null,
+        q1: '',
+        q2: '',
+        q3: '',
+        q4: '',
+        q5: '',
+        q6: '',
+        q7: '',
+        q8: '',
+        q9: '',
+        q10: ''
     }
 
     componentDidMount() {
@@ -261,113 +271,113 @@ class Form1 extends React.Component {
                         </FormGroup>
                     </Form>
                     <div>
-<h3><strong>Question 1</strong></h3>
-<h4>You enjoy playing the piano and writing chord prgressions</h4>
-<select name={'q1'} onChange={this.handleInputChange} className="chosen-select" id="q1">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 1</strong></h3>
+                        <h4>You enjoy playing the piano and writing chord prgressions</h4>
+                        <select name={'q1'} onChange={this.handleInputChange} className="chosen-select" id="q1">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
-<h3><strong>Question 2</strong></h3>
-<h4>You are an avid guitar player</h4>
-<select name={'q2'} onChange={this.handleInputChange} className="chosen-select" id="q2">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
-
-
-<h3><strong>Question 3</strong></h3>
-<h4>When it comes to playing the drums you are a natural</h4>
-<select name={'q3'} onChange={this.handleInputChange} className="chosen-select" id="q3">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 2</strong></h3>
+                        <h4>You are an avid guitar player</h4>
+                        <select name={'q2'} onChange={this.handleInputChange} className="chosen-select" id="q2">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
 
-<h3><strong>Question 4</strong></h3>
-<h4>You have a deep love for live instrumentation</h4>
-<select name={'q4'} onChange={this.handleInputChange} className="chosen-select" id="q4">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 3</strong></h3>
+                        <h4>When it comes to playing the drums you are a natural</h4>
+                        <select name={'q3'} onChange={this.handleInputChange} className="chosen-select" id="q3">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
 
-<h3><strong>Question 5</strong></h3>
-<h4>You consider yourself a vocalist</h4>
-<select name={'q5'} onChange={this.handleInputChange} className="chosen-select" id="q5">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 4</strong></h3>
+                        <h4>You have a deep love for live instrumentation</h4>
+                        <select name={'q4'} onChange={this.handleInputChange} className="chosen-select" id="q4">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
 
-<h3><strong>Question 6</strong></h3>
-<h4>Producing music and making beats in your passion</h4>
-<select name={'q6'} onChange={this.handleInputChange} className="chosen-select" id="q6">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 5</strong></h3>
+                        <h4>You consider yourself a vocalist</h4>
+                        <select name={'q5'} onChange={this.handleInputChange} className="chosen-select" id="q5">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
 
-<h3><strong>Question 7</strong></h3>
-<h4>Hip Hop and Rap is your favorite genre</h4>
-<select name={'q7'} onChange={this.handleInputChange} className="chosen-select" id="q7">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 6</strong></h3>
+                        <h4>Producing music and making beats in your passion</h4>
+                        <select name={'q6'} onChange={this.handleInputChange} className="chosen-select" id="q6">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
-<h3><strong>Question 8</strong></h3>
-<h4>Rock is your favorite genre</h4>
-<select name={'q8'} onChange={this.handleInputChange} className="chosen-select" id="q8">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
 
-<h3><strong>Question 9</strong></h3>
-<h4>You enjoy Electronic music and unique sounds</h4>
-<select name={'q9'} onChange={this.handleInputChange} className="chosen-select" id="q9">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 7</strong></h3>
+                        <h4>Hip Hop and Rap is your favorite genre</h4>
+                        <select name={'q7'} onChange={this.handleInputChange} className="chosen-select" id="q7">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
-<h3><strong>Question 10</strong></h3>
-<h4>You enjoy writing songs</h4>
-<select name={'q10'} onChange={this.handleInputChange} className="chosen-select" id="q10">
-    <option value="1">1 (Strongly Disagree)</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5 (Strongly Agree)</option>
-</select>
+                        <h3><strong>Question 8</strong></h3>
+                        <h4>Rock is your favorite genre</h4>
+                        <select name={'q8'} onChange={this.handleInputChange} className="chosen-select" id="q8">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
 
-</div>
-<br></br>
+                        <h3><strong>Question 9</strong></h3>
+                        <h4>You enjoy Electronic music and unique sounds</h4>
+                        <select name={'q9'} onChange={this.handleInputChange} className="chosen-select" id="q9">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
+
+                        <h3><strong>Question 10</strong></h3>
+                        <h4>You enjoy writing songs</h4>
+                        <select name={'q10'} onChange={this.handleInputChange} className="chosen-select" id="q10">
+                            <option value="1">1 (Strongly Disagree)</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5 (Strongly Agree)</option>
+                        </select>
+
+                    </div>
+                    <br></br>
 
                     <Button onClick={this.handleFormSubmit}>Submit</Button>
                 </Grid>

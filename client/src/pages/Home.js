b8827c1bcf8bd1f8150db4864f70f1f9f846d972
Bharti <tagra.bharti@gmail.com>
2019-01-31 19:34:39 -0600

    nothing changed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import { Nav, NavItem, Button, Navbar, Media, Grid, Row, Col, ListGroup, ListGro
 import API from '../utils/API';
 import './Home.css';
 import Icon from "../components/chat/Message-Icon/index";
+
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -102,7 +103,6 @@ class Home extends Component {
                             <NavItem >
                                 <Icon />
                             </NavItem>
-
                             <NavItem eventKey={1}>
                                 <Button onClick={this.logout}>Logout</Button>
                             </NavItem>

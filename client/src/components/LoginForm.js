cc315b944ec30b5766570e834e68d6b2a2268620
Dylan White <dylanawhite11@gmail.com>
2019-02-01 08:43:16 -0600

    formatting

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,8 +3,8 @@ import fire from '../config/Fire';
 import { Modal, Button, Form, FormGroup, ControlLabel, FormControl, Row, Col } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import API from '../utils/API';
-class LoginForm extends React.Component {
 
+class LoginForm extends React.Component {
     state = {
         email: '',
         password: '',
@@ -24,18 +24,19 @@ class LoginForm extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         if (!this.state.email || !this.state.password) {
-            alert("Fill your Email and Password please!");
+            alert("Please fill out your email and password!");
         }
         else if (this.state.password.length < 6) {
-            alert(`Password should be at least 6 characters`);
+            alert(`Password should be at least 6 characters.`);
         }
         else {
             if (this.props.type === "Login") {
                 fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
                     const uid = u.user.uid
+
                     localStorage.setItem("uid", uid)
                 }).catch((error) => {
-                    console.log(error);
+                    res.json(error);
                 });
             }
             else {
@@ -43,14 +44,16 @@ class LoginForm extends React.Component {
                     const { history } = this.props;
                     const uid = u.user.uid
                     const userData = { uid }
+
                     localStorage.setItem("uid", uid)
+
                     API.createUser(userData).then(
                         history.push('/form')
-                    )
+                    );
                 })
-                    .catch((error) => {
-                        console.log(error);
-                    })
+                .catch((error) => {
+                    res.json(error);
+                })
             }
         }
     }
@@ -68,10 +71,8 @@ class LoginForm extends React.Component {
     }
 
     render() {
-
         return (
             <Modal show={this.state.show} onHide={this.handleClose}>
-
                 <Modal.Header>
                     <Row className="show-grid">
                         <Col xs={9} md={8}>
@@ -96,7 +97,7 @@ class LoginForm extends React.Component {
                                 onChange={this.handleChange} />
                         </FormGroup>
                         <FormGroup controlId="formInlinePassword">
-                            <ControlLabel>Password(At least 6 characters)</ControlLabel>
+                            <ControlLabel>Password (At least 6 characters)</ControlLabel>
                             <FormControl
                                 type="password"
                                 name="password"
@@ -111,9 +112,8 @@ class LoginForm extends React.Component {
                     <Button onClick={this.handleSubmit} bsStyle="primary">{this.props.type}</Button>
                 </Modal.Footer>
             </Modal>
-        )
+        );
     }
-
 }
 
 export default withRouter(LoginForm);;

a0bcb02bbe5b41d2e0bcfbffb0a9e69b89542a64
Dylan White <dylanawhite11@gmail.com>
2019-02-01 08:40:18 -0600

    removing extra lines and console logs that aren't needed anymore

--- client/src/components/chat/Navbar-Chat/index.js
+++ client/src/components/chat/Navbar-Chat/index.js
@@ -12,55 +12,49 @@ class NavChat extends Component {
 
     }
 
-
-
     state = {
         uid: '',
         profiles: null
     };
 
     logout() {
-        // alert("Alert");
         fire.auth().signOut();
-        console.log(fire.auth().signOut());
-
     }
 
     componentDidMount() {
-        console.log("Here we are");
-
         const uid = localStorage.getItem("uid");
+
         this.fetchNotes(uid);
     }
 
     fetchNotes = (uid) => {
         API.getUser(uid)
-            .then(res => {
-                console.log(res.data[0].profiles[0]);
-                const profiles = res.data[0].profiles[0];
-                if (profiles !== 0) {
-                    this.setState({
-                        profiles
-                    });
-                }
-                console.log("state after profiles came back!", this.state);
-            });
-    }
+        .then(res => {
+            const profiles = res.data[0].profiles[0];
 
+            if (profiles !== 0) {
+                this.setState({ profiles });
+            }
+        });
+    }
 
     render() {
         return (
-
             <Navbar inverse collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        {this.state.profiles && <h1>{this.state.profiles.firstName} {this.state.profiles.lastName}</h1>}
+                        {
+                            this.state.profiles && 
+                            <h1>
+                                {this.state.profiles.firstName} 
+                                {this.state.profiles.lastName}
+                            </h1>
+                        }
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-
                         <NavItem className="icon-style">
                             <Icon />
                         </NavItem>
@@ -71,8 +65,6 @@ class NavChat extends Component {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-
-
         );
     }
 }
